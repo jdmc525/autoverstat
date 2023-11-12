7346d0854628a4ed604160d62da6c974df639747
jdmc525 <121909687+jdmc525@users.noreply.github.com>
2023-11-11 21:44:05 -0600

    Terminado boceto #2, inicio de media querys, problema principal es que el header se ve afectado por el carrusel

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let servicio = document.querySelector('.servicio');
+/*let servicio = document.querySelector('.servicio');
 
 servicio.addEventListener('mouseover', function () {
     console.log('Hola');
@@ -11,3 +11,25 @@ servicio.addEventListener('mouseout', function () {
     this.classList.add('border-top-bottom');
     this.classList.remove('border-left-right');
 });
+
+
+
+
+/********CARROUSEL**********/
+
+var carousel = document.getElementById('carousel');
+var slides = carousel.querySelectorAll('.carousel-slides li');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 3000);
+
+function prevSlide() {
+    slides[currentSlide].style.opacity = 0;
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].style.opacity = 1;
+}
+
+function nextSlide() {
+    slides[currentSlide].style.opacity = 0;
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.opacity = 1;
+}
